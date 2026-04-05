ca77fb19080a001f742ca68af1630f83f55e5217
mechaniqe <qaraxanli.sahin@gmail.com>
2026-04-05 10:31:37 +0400

    Integrated async methods

--- Runtime/SaveManager.cs
+++ Runtime/SaveManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading.Tasks;
 using System.Xml.Serialization;
 using UnityEngine;
 
@@ -50,6 +51,99 @@ namespace DynamicBox.SaveManagement
       {
         Debug.LogWarning("File writing error: " + ex.Message);
       }
+    }
+
+    public async Task SaveToFileAsync<T>(T dataToStore, string dataName)
+    {
+      string fileName = _savingLocation + "/" + dataName + "." + _method.ToString().ToLower();
+
+      try
+      {
+        switch (_method)
+        {
+          case StorageMethod.Binary:
+            await Task.Run(() =>
+            {
+              BinaryFormatter formatter = new BinaryFormatter();
+              using (FileStream stream = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+              {
+                formatter.Serialize(stream, dataToStore);
+              }
+            });
+            break;
+
+          case StorageMethod.XML:
+            await Task.Run(() =>
+            {
+              XmlSerializer serializer = new XmlSerializer(typeof(T));
+              using (FileStream stream = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+              {
+                serializer.Serialize(stream, dataToStore);
+              }
+            });
+            break;
+
+          case StorageMethod.JSON:
+            string serializedData = JsonUtility.ToJson(dataToStore, true);
+            using (StreamWriter writer = new StreamWriter(fileName, false))
+            {
+              await writer.WriteAsync(serializedData);
+            }
+            break;
+        }
+      }
+      catch (System.Exception ex)
+      {
+        Debug.LogWarning("File writing error: " + ex.Message);
+      }
+    }
+
+    public async Task<T> LoadFromFileAsync<T>(string dataName, T defaultValue)
+    {
+      string fileName = _savingLocation + "/" + dataName + "." + _method.ToString().ToLower();
+
+      try
+      {
+        switch (_method)
+        {
+          case StorageMethod.Binary:
+            return await Task.Run(() =>
+            {
+              using (Stream stream = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read))
+              {
+                BinaryFormatter formatter = new BinaryFormatter();
+                return (T) formatter.Deserialize(stream);
+              }
+            });
+
+          case StorageMethod.XML:
+            return await Task.Run(() =>
+            {
+              using (FileStream stream = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read))
+              {
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+                return (T) serializer.Deserialize(stream);
+              }
+            });
+
+          case StorageMethod.JSON:
+            string serializedData;
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+              serializedData = await reader.ReadToEndAsync();
+            }
+            return JsonUtility.FromJson<T>(serializedData);
+        }
+      }
+      catch (System.Exception ex)
+      {
+        Debug.LogWarning("File reading error: " + ex.Message);
+
+        await ResetDataAsync<T>(dataName, defaultValue);
+        return await LoadFromFileAsync<T>(dataName, defaultValue);
+      }
+
+      return defaultValue;
     }
 
     public bool FileExists(string dataName)
@@ -161,6 +255,11 @@ namespace DynamicBox.SaveManagement
       T initialValue = defaultValue;
 
       SaveToFile(initialValue, dataName);
+    }
+
+    private async Task ResetDataAsync<T>(string dataName, T defaultValue)
+    {
+      await SaveToFileAsync(defaultValue, dataName);
     }
 
     public void RemoveData()
